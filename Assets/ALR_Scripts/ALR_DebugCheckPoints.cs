6c3c760d59bf775a07e4033fde707be962a7dfad
Acanthaster <a.remazeilles@rubika-edu.com>
2020-06-02 17:05:27 +0200

    Update LD

--- Assets/ALR_Scripts/ALR_DebugCheckPoints.cs
+++ Assets/ALR_Scripts/ALR_DebugCheckPoints.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class ALR_DebugCheckPoints : MonoBehaviour
 {
 
-    Transform[] allChildren = new Transform[11];
+    [SerializeField] Transform[] allChildren;
 
     public Camera mainCamera;
     public GameObject avatar;
 
-    private int pointer = 1;
+    private int pointer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        allChildren = GetComponentsInChildren<Transform>();
+        //allChildren = GetComponentsInChildren<Transform>();
     }
 
     public void NextCheckPoint ()
     {
-        if(pointer < 11)
+        if(pointer < 10)
         {
 
             mainCamera.gameObject.transform.GetChild(0).gameObject.SetActive(false);
@@ -27,18 +27,22 @@ public class ALR_DebugCheckPoints : MonoBehaviour
             mainCamera.transform.position = allChildren[pointer + 1].transform.position;
             mainCamera.gameObject.transform.GetChild(0).gameObject.SetActive(true);
             pointer++;
+
+            Debug.Log("NEXT !" + avatar.transform.position);
         }
     }
 
     public void PreviousCheckPoint()
     {
-        if (pointer > 1)
+        if (pointer > 0)
         {
             mainCamera.gameObject.transform.GetChild(0).gameObject.SetActive(false);
             avatar.transform.position = allChildren[pointer - 1].transform.position;
             mainCamera.transform.position = allChildren[pointer - 1].transform.position;
             mainCamera.gameObject.transform.GetChild(0).gameObject.SetActive(true);
             pointer--;
+
+            Debug.Log("PREVIOUS !" + avatar.transform.position);
         }
     }
 }
